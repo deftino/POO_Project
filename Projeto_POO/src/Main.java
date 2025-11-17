8129af1264798d35fe7de5a03de8bf1e40233f3d
Guilherme <rancherized@gmail.com>
2025-11-17 18:53:57 -0300

    Delete Projeto_POO directory

--- Projeto_POO/src/Main.java
+++
@@ -1,9 +0,0 @@
-import login.pk.*;
-
-public class Main {
-    
-	public static void main(String[] args) {
-    	Autenticacao autenticacao = new Autenticacao();
-    	Paginalogin login = new Paginalogin(autenticacao.getlogininfo(), autenticacao);
-     }
-}
